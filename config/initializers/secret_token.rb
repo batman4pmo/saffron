f11cf2dcc8019b397068c869744c34bbc53c8eae
bmaher <briancmaher@gmail.com>
2011-03-30 00:39:20 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saffron::Application.config.secret_token = '6951da8f0498620ca81bc59c0379e889418d95f39a0bc021752e5e4d0077b9548936c4902138a66be0e2cd252c86911e57736a70fc18a19328908e18238a7903'
