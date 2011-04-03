860c838e8ae3ab547ec0ecc2de0066e36e6aee5e
bmaher <briancmaher@gmail.com>
2011-04-03 15:44:14 +0100

    Fixing OpenId initialisation for Heroku.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 #  provider :twitter, 'ZRNyeuetwQTYyXZ8CiCDg', 'Oyvq8eQZrSmB1wX8aEQRcWMBMDEAQml1FAtuMytjOVs'
-  provider :open_id, OpenID::Store::Filesystem.new('/tmp')
+  provider :open_id, OpenID::Store::Filesystem.new('./tmp')
 end
